6e43be1a7f51c97f20d08c3f4bf43308d4a9431a
Hoyoon Lee <wetet2@gmail.com>
2021-05-20 21:26:32 +0900

    마무리

--- client/components/common/ListFilters.style.js
+++ client/components/common/ListFilters.style.js
@@ -10,6 +10,9 @@ export const Select = styled.select`
    border: 0;
    font-size: 14px;
    outline: none;
+   appearance: none;
+   padding: 0 16px 0 8px;
+   background: url(/images/select-arrow.svg) no-repeat right / 11px;
 
    & + * {
       margin-left: 12px;
